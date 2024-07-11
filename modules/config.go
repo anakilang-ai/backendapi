eb3f2773070cbf5e6e63f102eb60ba5c0b6d9f96
raffzhm <rafiazhim.ra@gmail.com>
2024-07-12 00:54:40 +0700

    fix env

--- modules/config.go
+++ modules/config.go
@@ -3,11 +3,11 @@ package modules
 import (
 	"os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func GetEnv(envName string) string {
-	envFile, _ := godotenv.Read("./.env")
-	return envFile[envName]
+	// envFile, _ := godotenv.Read("./.env")
+	// return envFile[envName]
 	return os.Getenv(envName)
 }
